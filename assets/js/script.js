ef722abce0bdc2f1dde93a14a79f68e786c67864
CHANDAN RAJ <codeswithchandan@gmail.com>
2021-03-24 17:08:08 +0530

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,28 +17,21 @@ $(function(){
 
 
 $( document ).ready(function() {
-
-$('.doctors-slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<span class="prevArrow"><i class="fa fa-angle-left"></span>',
-  nextArrow: '<span class="nextArrow"><i class="fa fa-angle-right"></span>',
-  autoplay:true,
-  autoplaySpeed:3500,
-  arrows:true,
-  dots:false,
-  pauseOnHover:false,
-  infinite: true,
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1
-      }
     
-    }
-  ]
-});
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 
 });
 
