d26b8e294fec58be7c5eafddcc0812e55aa93551
Algopanda Technology <info@algopanda.com>
2021-04-05 15:14:56 +0530

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-$(function() {
-    $('.image-link').viewbox({
-        setTitle: true,
-        margin: 20,
-        resizeDuration: 300,
-        openDuration: 200,
-        closeDuration: 200,
-        closeButton: true,
-        fullscreenButton: false,
-        navButtons: true,
-        closeOnSideClick: true,
-        nextOnContentClick: false,
-        useGestures: true
-    });
-});
 
 
 $(document).ready(function() {
@@ -34,23 +19,6 @@ $(document).ready(function() {
 
 });
 
-
-jQuery(window).on("load", function() {
-    jQuery(window).scroll(function() {
-        var windowBottom = jQuery(this).scrollTop() + jQuery(this).innerHeight();
-        jQuery(".fade").each(function() {
-            /* Check the location of each desired element */
-            var objectBottom = jQuery(this).offset().top + jQuery(this).outerHeight();
-
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if (jQuery(this).css("opacity") == 0) { jQuery(this).fadeTo(500, 1); }
-            } else { //object goes out of view (scrolling up)
-                if (jQuery(this).css("opacity") == 1) { jQuery(this).fadeTo(500, 0); }
-            }
-        });
-    }).scroll(); //invoke scroll-handler on page-load
-});
 
 $(document).ready(function() {
 
